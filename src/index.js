b600d50063b2372b66716b9d99aa5b83ac635445
jonybang <Jonybange@gmail.com>
2019-06-26 20:37:10 +0200

    fix index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const geohash = require("./geohash.js");
 const geohashContour = require("./geohashContour.js");
 const geohashExtra = require("./geohashExtra.js");
 
-const latLon = require("./latLon.js");
 const utm = require("./utm.js");
 
 geohash.contour = geohashContour;
@@ -11,8 +10,6 @@ geohash.extra = geohashExtra;
 
 const all = common;
 all.geohash = geohash;
-
-all.latLon = latLon;
 all.utm = utm;
 
 module.exports = all;
