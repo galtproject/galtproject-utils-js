b103bf82b7665ffff7866f30655a9e23038a91f3
jonybang <Jonybange@gmail.com>
2019-12-14 04:41:29 +0200

    remove libs

--- src/latLon.js
+++ src/latLon.js
@@ -7,19 +7,8 @@
  * [Basic Agreement](ipfs/QmaCiXUmSrP16Gz8Jdzq6AJESY1EAANmmwha15uR3c1bsS)).
  */
 
-const rEarth = 6378;
 const Utm = require('./utm');
-const geolib = require('geolib');
-const mtLatlon = require('mt-latlon');
 const geolocationUtils = require('geolocation-utils');
-
-Number.prototype.toRad = function() {
-  return this * Math.PI / 180;
-}
-
-Number.prototype.toDeg = function() {
-  return this * 180 / Math.PI;
-}
 
 module.exports = class LatLon {
   static shift(latitude, longitude, dx, dy) {
@@ -33,8 +22,6 @@ module.exports = class LatLon {
     const zoneDegreeEast = (utmPoint.zone * 6) - 180;
     const zoneDegreeWest = zoneDegreeEast - 6;
 
-    // console.log('resultLatLon', resultLatLon);
-    // console.log('zoneDegree', zoneDegreeEast, zoneDegreeWest);
     let isResultUtmValid = resultLatLon.lon < zoneDegreeEast && resultLatLon.lon > zoneDegreeWest;
     if(isResultUtmValid) {
       return resultLatLon;
@@ -47,22 +34,6 @@ module.exports = class LatLon {
 
     // 25.010148443969
     return geolocationUtils.moveTo({lat: latitude, lon: longitude}, {heading: brng, distance: dist * 1000});
-
-    // 25.066212200798
-    // const point = new mtLatlon(latitude, longitude);
-    // const result = point.destinationPoint(brng,dist);
-    // console.log('result', result);
-    // return {
-    //   lat: result._lat,
-    //   lon: result._lon
-    // }
-    // 25.066212200798
-    // const result = geolib.computeDestinationPoint({ latitude, longitude }, dist * 1000, brng);
-    // console.log('result', result);
-    // return {
-    //   lat: result.latitude,
-    //   lon: result.longitude
-    // }
     // 25.011218773603
     // return {
     //   lat: latitude  + ((dy / 1000) / rEarth) * (180 / Math.PI),
