5a6e94f1b7472b7ac81266a7a4a8d23a1f8a8494
jonybang <jonybange@gmail.com>
2019-12-18 04:20:56 +0200

    latLon

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const geohashExtra = require("./geohashExtra.js");
 const contractPoint = require("./contractPoint.js");
 
 const utm = require("./utm.js");
+const latLon = require("./latLon.js");
 const coordinates = require("./coordinates.js");
 
 geohash.contour = geohashContour;
@@ -13,6 +14,7 @@ geohash.extra = geohashExtra;
 const all = common;
 all.geohash = geohash;
 all.utm = utm;
+all.latLon = latLon;
 all.coordinates = coordinates;
 all.contractPoint = contractPoint;
 
